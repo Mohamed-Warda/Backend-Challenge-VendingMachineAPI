c3d2106ed58d9ae3fa983cfb8c93507c2fa1f35d
Mohamed Warda <mohmdwarda@gmail.com>
2024-02-10 23:43:54 +0200

    enhancements

--- VendingMachine.API/Controllers/UserController.cs
+++ VendingMachine.API/Controllers/UserController.cs
@@ -17,18 +17,55 @@ namespace VendingMachine.API.Controllers
     {
         private readonly IAuthService authService;
         private readonly IUserService userService;
+        private readonly ILogger<UserController> _logger;
 
-        public UserController(IAuthService authService, IUserService userService)
+
+        public UserController(IAuthService authService, IUserService userService, ILogger<UserController> logger)
         {
             this.authService = authService;
             this.userService = userService;
+            _logger = logger;
         }
         [HttpPost, Route("Register")]
         public async Task<IActionResult> Register(RegisterDto user)
         {
-            if (ModelState.IsValid)
+            try
             {
-                var result = await authService.RegisterAsync(user);
+                _logger.LogInformation($"Invoking 'Register' EndPoint in 'UserController'");
+                if (ModelState.IsValid)
+                {
+                    var result = await authService.RegisterAsync(user);
+                    if (result is null || !result.IsAuthenticated)
+                    {
+                        return BadRequest();
+                    }
+                    else
+                    {
+                        return Ok(result);
+                    }
+                }
+                else
+                {
+                    return BadRequest(ModelState);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error When Invoking 'Register' EndPoint in 'UserController' : {ex.Message} ");
+                return StatusCode(500, "Internal Server Error");
+
+            }
+
+        }
+        [HttpPost, Route("Login")]
+
+        public async Task<IActionResult> Login([FromBody] LoginDto user)
+        {
+            try
+            {
+                _logger.LogInformation($"Invoking 'Login' EndPoint in 'UserController'");
+                var result = await authService.GetTokenAsync(user);
+
                 if (result is null || !result.IsAuthenticated)
                 {
                     return BadRequest();
@@ -38,24 +75,11 @@ namespace VendingMachine.API.Controllers
                     return Ok(result);
                 }
             }
-            else
+            catch (Exception ex)
             {
-                return BadRequest(ModelState);
-            }
-        }
-        [HttpPost, Route("Login")]
+                _logger.LogError($"Error When Invoking 'Login' EndPoint in 'UserController' : {ex.Message} ");
+                return StatusCode(500, "Internal Server Error");
 
-        public async Task<IActionResult> Login([FromBody] LoginDto user)
-        {
-            var result = await authService.GetTokenAsync(user);
-
-            if (result is null || !result.IsAuthenticated)
-            {
-                return BadRequest();
-            }
-            else
-            {
-                return Ok(result);
             }
         }
 
@@ -64,14 +88,24 @@ namespace VendingMachine.API.Controllers
         [Authorize(Roles = Roles.Admin)]
         public async Task<IActionResult> DeleteUserById([FromQuery] int id)
         {
-            var result = await userService.DeleteUserAsync(id);
-            if (result)
+            try
             {
-                return Ok("User Deleted");
+                _logger.LogInformation($"Invoking 'DeleteUserById' EndPoint in 'UserController'");
+                var result = await userService.DeleteUserAsync(id);
+                if (result)
+                {
+                    return Ok("User Deleted");
+                }
+                else
+                {
+                    return BadRequest();
+                }
             }
-            else
+            catch (Exception ex)
             {
-                return BadRequest();
+                _logger.LogError($"Error When Invoking 'DeleteUserById' EndPoint in 'UserController' : {ex.Message} ");
+                return StatusCode(500, "Internal Server Error");
+
             }
 
         }
@@ -81,8 +115,18 @@ namespace VendingMachine.API.Controllers
         [Authorize(Roles = Roles.Admin)]
         public async Task<IActionResult> GetAllUser()
         {
-            var users = await userService.GetAllUsers();
-            return Ok(users);
+            try
+            {
+                _logger.LogInformation($"Invoking 'GetAllUser' EndPoint in 'UserController'");
+                var users = await userService.GetAllUsers();
+                return Ok(users);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error When Invoking 'GetAllUser' EndPoint in 'UserController' : {ex.Message} ");
+                return StatusCode(500, "Internal Server Error");
+
+            }
         }
     }
 }
